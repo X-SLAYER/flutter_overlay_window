97de5348461aaff02c2ce017af6d075afc36898b
Ahmed Radwan Ahmed <38192189+ahmedradwan18@users.noreply.github.com>
2022-12-22 17:16:53 +0200

    Update overlay_config.dart

--- lib/overlay_config.dart
+++ lib/overlay_config.dart
@@ -1,3 +1,141 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:flutter/services.dart';
+
+/// default size when the overlay match the parent size
+/// basically it will take the full screen width and height
+const int matchParent = -1;
+
+class FlutterOverlayWindow {
+  FlutterOverlayWindow._();
+
+  static final StreamController _controller = StreamController();
+  static const MethodChannel _channel =
+      MethodChannel("x-slayer/overlay_channel");
+  static const MethodChannel _overlayChannel =
+      MethodChannel("x-slayer/overlay");
+  static const BasicMessageChannel _overlayMessageChannel =
+      BasicMessageChannel("x-slayer/overlay_messenger", JSONMessageCodec());
+
+  /// Open overLay content
+  ///
+  /// - Optional arguments:
+  /// `height` the overlay height and default is [matchParent]
+  /// `width` the overlay width and default is [matchParent]
+  /// `alignment` the alignment postion on screen and default is [OverlayAlignment.center]
+  /// `visibilitySecret` the detail displayed in notifications on the lock screen and default is [NotificationVisibility.visibilitySecret]
+  /// `OverlayFlag` the overlay flag and default is [OverlayFlag.defaultFlag]
+  /// `overlayTitle` the notification message and default is "overlay activated"
+  /// `overlayContent` the notification message
+  /// `enableDrag` to enable/disable dragging the overlay over the screen and default is "false"
+  /// `positionGravity` the overlay postion after drag and default is [PositionGravity.none]
+  static Future<void> showOverlay({
+    int height = matchParent,
+    int width = matchParent,
+    OverlayAlignment alignment = OverlayAlignment.center,
+    NotificationVisibility visibility = NotificationVisibility.visibilitySecret,
+    OverlayFlag flag = OverlayFlag.defaultFlag,
+    String overlayTitle = "overlay activated",
+    String? overlayContent,
+    bool enableDrag = false,
+    PositionGravity positionGravity = PositionGravity.none,
+  }) async {
+    await _channel.invokeMethod(
+      'showOverlay',
+      {
+        "height": height,
+        "width": width,
+        "alignment": alignment.name,
+        "flag": flag.name,
+        "overlayTitle": overlayTitle,
+        "overlayContent": overlayContent,
+        "enableDrag": enableDrag,
+        "notificationVisibility": visibility.name,
+        "positionGravity": positionGravity.name,
+      },
+    );
+  }
+
+  /// Check if overlay permission is granted
+  static Future<bool> isPermissionGranted() async {
+    try {
+      return await _channel.invokeMethod<bool>('checkPermission') ?? false;
+    } on PlatformException catch (error) {
+      log("$error");
+      return Future.value(false);
+    }
+  }
+
+  /// Request overlay permission
+  /// it will open the overlay settings page and return `true` once the permission granted.
+  static Future<bool?> requestPermission() async {
+    try {
+      return await _channel.invokeMethod<bool?>('requestPermission');
+    } on PlatformException catch (error) {
+      log("Error requestPermession: $error");
+      rethrow;
+    }
+  }
+
+  /// Closes overlay if open
+  static Future<bool?> closeOverlay() async {
+    // context.startService(i);
+    // result.success(true);
+    // if (OverlayService.isRunning) {
+    //   final Intent i = new Intent(context, OverlayService());
+    //   i.putExtra(OverlayService.INTENT_EXTRA_IS_CLOSE_WINDOW, true);
+    // }
+    // return;
+    final bool? _res = await _overlayChannel.invokeMethod('close');
+    return _res;
+  }
+
+  /// Broadcast data to and from overlay app
+  static Future shareData(dynamic data) async {
+    return await _overlayMessageChannel.send(data);
+  }
+
+  /// Streams message shared between overlay and main app
+  static Stream<dynamic> get overlayListener {
+    _overlayMessageChannel.setMessageHandler((message) async {
+      _controller.add(message);
+      return message;
+    });
+    return _controller.stream;
+  }
+
+  /// Update the overlay flag while the overlay in action
+  static Future<bool?> updateFlag(OverlayFlag flag) async {
+    final bool? _res = await _overlayChannel
+        .invokeMethod<bool?>('updateFlag', {'flag': flag.name});
+    return _res;
+  }
+
+  /// Update the overlay size in the screen
+  static Future<bool?> resizeOverlay(int width, int height) async {
+    final bool? _res = await _overlayChannel.invokeMethod<bool?>(
+      'resizeOverlay',
+      {
+        'width': width,
+        'height': height,
+      },
+    );
+    return _res;
+  }
+
+  /// Check if the current overlay is active
+  static Future<bool> isActive() async {
+    final bool? _res = await _channel.invokeMethod<bool?>('isOverlayActive');
+    return _res ?? false;
+  }
+
+  /// Dispose overlay stream
+  static void disposeOverlayListener() {
+    _controller.close();
+  }
+}
+
 /// Placement of overlay within the screen.
 enum OverlayAlignment {
   topLeft,
@@ -65,16 +203,4 @@ enum NotificationVisibility {
 
   /// Show this notification on all lockscreens, but conceal sensitive or private information on secure lockscreens.
   visibilityPrivate
-}
-
-class WindowSize {
-  WindowSize._();
-
-  /// default size when the overlay match the parent size
-  /// basically it will take the full screen width and height
-  static const int matchParent = -1;
-
-  /// make the overlay cover the fullscreen
-  /// even the statusbar and the navigationbar
-  static const int fullCover = -1999;
 }
